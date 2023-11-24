63dff873f42a6bd4a918eae424b9a839cc4e828a
kuryart <kuryart1@gmail.com>
2023-11-24 23:17:53 +0000

    adiciona doctype ao html e altera o titulo da pagina

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -1,50 +1,31 @@
 import { liveReloadScript } from "beth-stack/dev";
 import { type PropsWithChildren } from "beth-stack/jsx";
 import { config } from "../config";
+import { Doctype } from "./doctype";
 
 const safeScript =
   config.env.NODE_ENV === "development" ? liveReloadScript() : "";
 
-// export const BaseHtml = ({ children }: PropsWithChildren) => (
-//   <html data-theme="dim">
-//     <head>
-//       <meta charset="UTF-8" />
-//       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//       <title>THE BETH STACK</title>
-//       <script src="https://unpkg.com/htmx.org@1.9.5"></script>
-//       <script src="https://unpkg.com/htmx.org/dist/ext/response-targets.js"></script>
-//       <script src="https://unpkg.com/hyperscript.org@0.9.11"></script>
-
-//       <link rel="stylesheet" href="/public/dist/style.css" />
-
-//       <script>{safeScript}</script>
-//     </head>
-//     <body hx-boost="true" class="box-sizing-border m-0 p-0">
-//       {children}
-//     </body>
-//   </html>
-// );
-
-export const BaseHtml = ({ children }: PropsWithChildren) => {
-  const htmlContent = (
-    <html data-theme="dim">
+export const BaseHtml = ({ children }: PropsWithChildren) => (
+  // @ts-expect-error (can't inject Doctype as JSX element)
+  <Doctype>
+    <html data-theme="dim" lang="pt-br">
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>THE BETH STACK</title>
+        <meta name="description" content="Put your description here." />
+        <title>Paulo Cury | Portfólio</title>
         <script src="https://unpkg.com/htmx.org@1.9.5"></script>
         <script src="https://unpkg.com/htmx.org/dist/ext/response-targets.js"></script>
         <script src="https://unpkg.com/hyperscript.org@0.9.11"></script>
 
         <link rel="stylesheet" href="/public/dist/style.css" />
 
-        <script>${safeScript}</script>
+        <script>{safeScript}</script>
       </head>
       <body hx-boost="true" class="box-sizing-border m-0 p-0">
-        ${children}
+        {children}
       </body>
     </html>
-  );
-
-  return htmlContent;
-};
+  </Doctype>
+);
